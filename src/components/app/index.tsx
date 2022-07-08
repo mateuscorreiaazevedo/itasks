cb5d8903e0c1ef417ed52e0ac8e180b263cd18f2
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-07-08 17:39:30 -0300

    organize the App component

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,13 +1,13 @@
 import { localStorageTheme } from '../../utils/localStorageTheme'
 import { DefaultTheme, ThemeProvider } from 'styled-components'
 import GlobalStyle from '../../assets/style/global.style'
-import { SocialButtons } from '../footer/social-button'
 import light from '../../assets/style/theme/light'
 import dark from '../../assets/style/theme/dark'
+import { MainPage } from '../../pages/main-page'
+import { SocialButtons } from '../social-button'
 import { Header } from '../header'
 import { Footer } from '../footer'
 import React from 'react'
-import { MainPage } from '../../pages/main-page'
 
 export default function App () {
   const [theme, setTheme] = localStorageTheme<DefaultTheme>('theme', light)
