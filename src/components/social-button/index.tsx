673433ad4ac4452ff54356dd047e47bdcacfa96d
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-07-09 00:09:46 -0300

    chore: add .env arquive

--- src/components/social-button/index.tsx
+++ src/components/social-button/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BsLinkedin, BsInstagram, BsGithub, BsJustify } from 'react-icons/bs'
 import * as S from './style'
+import env from '../../config/env'
 
 export const SocialButtons = () => {
   const [links, setLinks] = React.useState<boolean>(false)
@@ -21,19 +22,19 @@ export const SocialButtons = () => {
         <div>
           <div className='btn'>
             <span className='btn-alt'>LinkedIn</span>
-            <a href="https://www.linkedin.com/in/mateuscorreiaazevedo" target='_blank' rel="noreferrer">
+            <a href={env.LINKEDIN} target='_blank' rel="noreferrer">
               <BsLinkedin />
             </a>
           </div>
           <div className="btn">
             <span className='btn-alt'>Instagram</span>
-            <a href="https://www.instagram.com/mateuscorreiaazevedo/" target='_blank' rel="noreferrer">
+            <a href={env.INSTAGRAM} target='_blank' rel="noreferrer">
               <BsInstagram />
             </a>
           </div>
           <div className="btn">
             <span className='btn-alt'>Repositore</span>
-            <a href="https://github.com/mateuscorreiaazevedo/itasks" target='_blank' rel="noreferrer">
+            <a href={env.GITHUB} target='_blank' rel="noreferrer">
               <BsGithub />
             </a>
           </div>
---
+++ src/config/env.ts
@@ -0,0 +1,5 @@
+export default {
+  LINKEDIN: import.meta.env.VITE_LINK_LINKEDIN,
+  INSTAGRAM: import.meta.env.VITE_LINK_INSTAGRAM,
+  GITHUB: import.meta.env.VITE_LINK_GITHUB
+}
