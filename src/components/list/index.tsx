eff797ccee1d0b40027f3fd061d487ff0b25a891
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-07-21 02:45:22 -0300

    feat: Task edit function and individual options menu implemented

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,8 +1,8 @@
 import { useAlteredTask } from '../../hooks/useAlteredTask'
-import { AiOutlineClose } from 'react-icons/ai'
 import { Tasks } from '../../types/tasks'
 import * as S from './style'
 import React from 'react'
+import { OptionButton } from './option-button'
 
 interface Props {
   tasks: Tasks[]
@@ -10,27 +10,53 @@ interface Props {
 }
 
 export const TaskList: React.FC<Props> = ({ setReload, tasks }) => {
-  const { checkedTask, removeTask } = useAlteredTask({
-    refresh: setReload,
-    tasks
-  })
+  const [taskEdited, setTaskEdited] = React.useState<string>('')
+  const {
+    checkedTask,
+    removeTask,
+    setViewEdit,
+    viewEdit,
+    toEnter
+  } = useAlteredTask({ refresh: setReload, tasks, taskEdited })
+
+  const activeEditField = (id: string) => {
+    setViewEdit(prev => prev === id ? null : id)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, task: string) => {
+    const { value } = e.target
+    setTaskEdited(value)
+  }
 
   return (
     <>
       {tasks.map(task => (
         <S.TaskContainer key={task.id}>
-          <div>
+          <div className='content'>
             <input
+              className='checkbox'
               onClick={(e) => checkedTask(e, task.id)}
               defaultChecked={task.done}
               type="checkbox"
               id={task.id}
             />
-            <S.TaskText isChecked={task.done} htmlFor={task.id}>{task.task}</S.TaskText>
+            {viewEdit === task.id
+              ? (
+                <S.TaskInput
+                  type="text"
+                  defaultValue={task.task}
+                  onChange={(e) => handleChange(e, task.task)}
+                  onKeyDown={(e) => toEnter(e, task.id)}
+                />
+                )
+              : <S.TaskText isChecked={task.done} htmlFor={task.id}>{task.task}</S.TaskText>
+            }
           </div>
-          <S.TaskBtn onClick={() => removeTask(task.id)}>
-            <AiOutlineClose />
-          </S.TaskBtn>
+          <OptionButton
+            removeTask={() => removeTask(task.id)}
+            taskEdit={() => activeEditField(task.id)}
+            id={task.id}
+          />
         </S.TaskContainer>
       ))}
     </>
--- src/hooks/useAlteredTask.ts
+++ src/hooks/useAlteredTask.ts
@@ -1,13 +1,16 @@
-import { Dispatch, MouseEvent, SetStateAction } from 'react'
-import { Tasks } from '../types/tasks'
+import { Dispatch, KeyboardEvent, MouseEvent, SetStateAction, useState } from 'react'
 import { setTaskStorage } from '../utils/setLocalStorage'
+import { Tasks } from '../types/tasks'
 
 interface Props {
   tasks: Tasks[]
   refresh: Dispatch<SetStateAction<boolean>>
+  taskEdited: string
 }
 
-export function useAlteredTask ({ tasks, refresh }: Props) {
+export function useAlteredTask ({ tasks, refresh, taskEdited }: Props) {
+  const [viewEdit, setViewEdit] = useState<string | null>(null)
+
   const checkedTask = (e: MouseEvent<HTMLInputElement>, id: string | number) => {
     const taskIndex = tasks.findIndex(item => item.id === id)
 
@@ -26,8 +29,26 @@ export function useAlteredTask ({ tasks, refresh }: Props) {
     refresh(prev => !prev)
   }
 
+  const toEnter = (e: KeyboardEvent<HTMLInputElement>, id: string) => {
+    const enter = e.code === 'Enter'
+    const findTask = tasks.findIndex(item => item.id === id)
+    if (enter) {
+      tasks[findTask] = {
+        ...tasks[findTask],
+        task: taskEdited
+      }
+
+      setTaskStorage(tasks)
+      refresh(prev => !prev)
+      setViewEdit(null)
+    }
+  }
+
   return {
     checkedTask,
-    removeTask
+    removeTask,
+    viewEdit,
+    setViewEdit,
+    toEnter
   }
 }
